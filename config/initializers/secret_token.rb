5b1886599b96a004368be24dccb7cb791bc3e709
Ruben Espinosa <rderoldan1@gmail.com>
2011-09-26 23:17:37 -0500

    first webpage with stylesheets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sps::Application.config.secret_token = 'e69469efc7845b240f01cac9ce47eb612e9df1dc6b6c1e8127716b400ddaea83c63b4ee4df48bfacd1b171a5f9c095e0d6ebbedef0c9797d938860628bf88716'
